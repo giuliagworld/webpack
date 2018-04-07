4ea800a336a934d85f76d7144beaa17fe9a95609
Training <training@Giulias-MBP.connect>
2018-04-07 20:40:00 +0100

    changed to ES6

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-const React = require("react");
-const ReactDOM = require("react-dom");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './scss/styles.scss';
 
 ReactDOM.render(
   <h1>Ciao, world!</h1>,
